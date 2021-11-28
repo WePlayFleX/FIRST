a8967e0be496e5ccfd8dfd506b87c5912e9c2fda
WePlayFleX <alex.kamai@ya.ru>
2021-11-28 21:39:18 +0300

    Добавьте файлы проекта.

---
+++ FIRST.cpp
@@ -0,0 +1,12 @@
+﻿//comments.cpp
+//демонстрирует использование комментариев
+#include <iostream>			//директива препроцессора
+
+using namespace std;		//директива using
+
+int main()					//функция с именем main
+{							//начало тела функции
+	cout << "Each era has its own language\n";		//оператор
+	system("pause");		
+	return 0;				//оператор
+}							//конец тела функции
